c9e5ebe5389f97b8b3a1b7c421ea7515f3d4bd53
chris-maroudas <mallibu97@gmail.com>
2013-08-01 00:50:52 +0300

    convert intentation to spaces

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,62 +1,62 @@
 class ReviewsController < ApplicationController
 
-	before_filter :signed_in_user, only: [:new, :create, :edit, :update, :destroy]
-	before_filter :correct_user, only: [:edit, :update, :destroy] #defines also an instance var for those
+  before_filter :signed_in_user, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :correct_user, only: [:edit, :update, :destroy] #defines also an instance var for those
 
-	def new
-		@review = Review.new
-	end
+  def new
+    @review = Review.new
+  end
 
-	def create
-		@review = current_user.reviews.build(params[:review])
-		if @review.save
-			redirect_to root_path
-			flash[:success] = "Thank you for your review"
-		else
-			render 'new'
-		end
-	end
+  def create
+    @review = current_user.reviews.build(params[:review])
+    if @review.save
+      redirect_to root_path
+      flash[:success] = "Thank you for your review"
+    else
+      render 'new'
+    end
+  end
 
-	def index
-		@reviews = Review.includes(:user).paginate(page: params[:page], per_page: 4) # avoid N+1 query
-	end
+  def index
+    @reviews = Review.includes(:user).paginate(page: params[:page], per_page: 4) # avoid N+1 query
+  end
 
-	def edit
-		#Instance var @review, defined at correct_user
-	end
+  def edit
+    #Instance var @review, defined at correct_user
+  end
 
-	def show
-		@review = Review.find(params[:id])
-		@comment = @review.comments.build # Initializing @comment for the comments/form partial
-		@comments = @review.comments.includes(:user)
-	end
+  def show
+    @review = Review.find(params[:id])
+    @comment = @review.comments.build # Initializing @comment for the comments/form partial
+    @comments = @review.comments.includes(:user)
+  end
 
-	def update
-		if @review.update_attributes(params[:review])
-			redirect_to root_path
-			flash[:success] = "Edit successful"
-		else
-			render 'edit'
-		end
-	end
+  def update
+    if @review.update_attributes(params[:review])
+      redirect_to root_path
+      flash[:success] = "Edit successful"
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@review.destroy
-		redirect_to root_path
-		flash[:notice] = 'Review deleted'
-	end
+  def destroy
+    @review.destroy
+    redirect_to root_path
+    flash[:notice] = 'Review deleted'
+  end
 
-	def category
-		@reviews = Review.includes(:user).where(category: params[:category]).paginate(page: params[:page], per_page: 1)
-	end
+  def category
+    @reviews = Review.includes(:user).where(category: params[:category]).paginate(page: params[:page], per_page: 1)
+  end
 
-	def search
-		@reviews = Review.search(params[:search])
-	end
+  def search
+    @reviews = Review.search(params[:search])
+  end
 
-	def correct_user
-		@review = current_user.reviews.find_by_id(params[:id]) # Check if the accessed review belongs to current user
-		redirect_to root_path, notice: 'You are not authorized for that action' if @review.nil?
-	end
+  def correct_user
+    @review = current_user.reviews.find_by_id(params[:id]) # Check if the accessed review belongs to current user
+    redirect_to root_path, notice: 'You are not authorized for that action' if @review.nil?
+  end
 
 end
